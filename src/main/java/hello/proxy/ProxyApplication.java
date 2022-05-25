9a002d2baa4be2fa4fa743529a065fce2f02f13f
woals4815 <ckrt4815@gmail.com>
2022-05-25 14:51:25 +0900

    [Feature] add v1 app and v1 config

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,17 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+/**
+ * base component scan package == app
+ * app config는 따로 import로 지정해줌
+ */
+@Import(AppV1Config.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+
 public class ProxyApplication {
 
 	public static void main(String[] args) {
