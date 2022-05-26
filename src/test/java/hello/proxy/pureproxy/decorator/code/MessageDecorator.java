0de243c6d74608de00a5d0e3f3e49058276cfdf7
woals4815 <ckrt4815@gmail.com>
2022-05-26 21:19:27 +0900

    [Feature] add decorator pattern 2

---
+++ src/test/java/hello/proxy/pureproxy/decorator/code/MessageDecorator.java
@@ -0,0 +1,24 @@
+package hello.proxy.pureproxy.decorator.code;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MessageDecorator implements Component {
+
+    private Component component;
+
+    public MessageDecorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String operation() {
+        log.info("MessageDecorator start!");
+
+        String result = component.operation();
+        String decoResult = "*****" + result + "*****";
+        log.info("MessageDecorator not applied={}, applied={}", result, decoResult);
+        return decoResult;
+    }
+}
--- src/test/java/hello/proxy/pureproxy/decorator/code/RealComponent.java
+++ src/test/java/hello/proxy/pureproxy/decorator/code/RealComponent.java
@@ -3,7 +3,8 @@ package hello.proxy.pureproxy.decorator.code;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class RealComponent implements Component{
+public class RealComponent implements Component {
+
     @Override
     public String operation() {
         log.info("RealComponent start!");
