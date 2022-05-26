f5ee5286bbd8e8362a8cde63606d9919eb8bc566
woals4815 <ckrt4815@gmail.com>
2022-05-27 00:37:40 +0900

    [Feature] add Proxy pattern applied code

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -2,21 +2,30 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 /**
  * base component scan package == app
  * app config는 따로 import로 지정해줌
  */
-@Import({AppV2Config.class, AppV1Config.class})
+//@Import({AppV2Config.class, AppV1Config.class})
+	@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 
 public class ProxyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+	@Bean
+	public LogTrace logTrace() {
+		return new ThreadLocalLogTrace();
 	}
 
 }
